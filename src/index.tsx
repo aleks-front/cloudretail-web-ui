74392b86eb9bf79f4ebecb49096d12fae6812a30
Ravshan Samandarov <ravshansbox@gmail.com>
2020-01-28 01:44:20 +0500

    Import bootstrap modules separately

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import 'bootstrap-scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
 ReactDOM.render(<h1>Cloud Retail</h1>, document.getElementById('app'));
