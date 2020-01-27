cb4a501fe41aaa9e34c017ec5038200f33f931c2
Ravshan Samandarov <ravshansbox@gmail.com>
2020-01-28 01:19:03 +0500

    Add css related stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -10,9 +11,26 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({ filename: 'index.css' }),
+  ],
   module: {
-    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }],
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      { test: /\.(ts|tsx)$/, use: 'ts-loader' },
+    ],
+  },
+  performance: {
+    maxAssetSize: 256 * 1024,
+    maxEntrypointSize: 512 * 1024,
   },
   stats: {
     children: false,
