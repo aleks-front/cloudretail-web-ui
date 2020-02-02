2b4b480342b06581102ab3998883b367f2a786a9
Ravshan Samandarov <ravshansbox@gmail.com>
2020-02-02 15:54:11 +0500

    chore: Bump deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import './index.scss';
 
-ReactDOM.render(<h1>Cloud Retail</h1>, document.getElementById('app'));
+const App = () => <h1>Cloud Retail</h1>;
+
+const store = createStore(state => state);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('app'),
+);
