df548a76bb551e57a98cfb18564fd5ee071a812a
Ravshan Samandarov <ravshansbox@gmail.com>
2020-04-16 06:18:50 +0500

    fix: Use Roboto font family

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BuildInfo } from './BuildInfo';
+import { GlobalStyles } from './GlobalStyles';
 
 export const App = () => (
   <>
-    <h1>Cloud Retail</h1>
+    <GlobalStyles />
     <BuildInfo />
+    <h1>Cloud Retail</h1>
   </>
 );
--- src/components/BuildInfo.tsx
+++ src/components/BuildInfo.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { build_time } from '../build.json';
 
-const Container = styled.div`
+const Container = styled.h6`
   position: absolute;
   right: 0;
   bottom: 0;
+  margin: 0;
 `;
 
 export const BuildInfo = () => (
