e60b2125ea804fd0117a2aa109d2f6ee0cb97a44
Ravshan Samandarov <ravshansbox@gmail.com>
2020-04-19 15:53:18 +0500

    fix: Remove auth token if it is invalid

--- src/sagas/signInSaga.ts
+++ src/sagas/signInSaga.ts
@@ -1,6 +1,7 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { signIn } from '../api/signIn';
+import { authTokenKey } from '../constants';
 import { history } from '../history';
 import { authActions, SignInRequestPayload } from '../slices/authSlice';
 
@@ -8,7 +9,7 @@ export function* signInSaga({ payload }: PayloadAction<SignInRequestPayload>) {
   try {
     const result = yield call(signIn, payload);
     yield put(authActions.signInSuccess());
-    localStorage.setItem('auth_token', result.id);
+    localStorage.setItem(authTokenKey, result.id);
     history.push('/');
   } catch (error) {
     yield put(authActions.signInFailure(error));
--- src/sagas/validatePreviousSessionSaga.ts
+++ src/sagas/validatePreviousSessionSaga.ts
@@ -1,10 +1,11 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { validatePreviousSession } from '../api/validatePreviousSession';
+import { authTokenKey } from '../constants';
 import { authActions } from '../slices/authSlice';
 
 export function* validatePreviousSessionSaga() {
   yield put(authActions.disableValidationRequire());
-  const authToken = localStorage.getItem('auth_token');
+  const authToken = localStorage.getItem(authTokenKey);
   if (authToken) {
     yield put(authActions.fetchAuthTokenRequest());
     try {
@@ -12,6 +13,7 @@ export function* validatePreviousSessionSaga() {
       yield put(authActions.fetchAuthTokenSuccess());
     } catch (error) {
       yield put(authActions.fetchAuthTokenFailure());
+      localStorage.removeItem(authTokenKey);
     }
   }
 }
